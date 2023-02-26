f2ee294dc514e81235539738a58cc246cbee3413
Raid Benakcha <kichoukh00@gmail.com>
2023-02-27 00:50:11 +0100

    ajouter validation du formulaire

--- frontend/src/components/Annonce/Annonce.tsx
+++ frontend/src/components/Annonce/Annonce.tsx
@@ -1,167 +1,297 @@
-import { Container } from '@mui/system'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
-import { useState } from 'react'
-import { Button, FormControl, Grid, Input, Typography } from '@mui/material'
-import './annonces.css'
-import AddIcon from '@mui/icons-material/Add'
-import Paper from '@mui/material/Paper'
-import Box from '@mui/material/Box'
+import { Container } from "@mui/system";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useEffect } from "react";
+import { Button, FormControl, Grid, Typography } from "@mui/material";
+import "./annonces.css";
+import AddIcon from "@mui/icons-material/Add";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import { TypeOf, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TextField } from "@mui/material";
+
 const Annonce = () => {
-  const [value, setValue] = useState('')
+  const createAnnonceSchema = z.object({
+    titre: z
+      .string()
+      .min(1, {
+        message: "Il faut un titre pour l'annonce",
+      })
+      .max(150, { message: "Vous ne devez pas dépasser 150 caractères" }),
+    description: z
+      .string({ required_error: "Il faut une description pour l'annonce" })
+      .min(1, {
+        message: "Il faut une description pour l'annonce",
+      })
+      .max(1500, { message: "Vous ne devez pas dépasser 1500 caractères" }),
+    adresse: z
+      .string()
+      .min(1, {
+        message: "Veuillez indiquer une adresse",
+      })
+      .max(200, { message: "Vous ne devez pas dépasser 200 caractères" }),
+    complement_adresse: z
+      .string()
+      .max(200, { message: "Vous ne devez pas dépasser 200 caractères" })
+      .optional(),
+    code_postal: z
+      .string()
+      .regex(/^(?:0[1-9]|[1-8]\d|9[0-8])\d{3}$/, {
+        message: "Code postal erroné",
+      })
+      .min(5)
+      .max(5),
+    ville: z
+      .string()
+      .min(1, { message: "Veuillez indiquer votre ville" })
+      .max(40, { message: "Vous ne devez pas dépasser 40 caractères" }),
+    telephone: z
+      .string()
+      .regex(/^0(6|7|9)\d{8}$/, { message: "Le numéro est erroné" }),
+  });
+  type CreateAnnonceInput = TypeOf<typeof createAnnonceSchema>;
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+    setValue,
+  } = useForm<CreateAnnonceInput>({
+    resolver: zodResolver(createAnnonceSchema),
+  });
+
+  useEffect(() => {
+    register("description");
+  }, [register]);
+  const description = watch("description");
+  const setDescription = (description: string) => {
+    setValue("description", description);
+  };
   return (
     <Container
-      sx={{ margin: '1rem auto' }}
-      component='form'
-      onSubmit={(e) => {
-        e.preventDefault()
-        console.log('submit')
-      }}
+      sx={{ margin: "1rem auto" }}
+      component="form"
+      onSubmit={handleSubmit(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )}
     >
-      <Box className='annonce-item'>
-        <FormControl className='annonce-item-element'>
-          <Input
-            style={{ fontSize: '1.5rem' }}
-            placeholder="Titre de l'annonce"
+      <Box className="annonce-item">
+        <FormControl className="annonce-item-element">
+          <TextField
+            error={!!errors.titre}
+            helperText={errors.titre?.message}
+            label="Titre de l'annonce"
+            variant="standard"
+            inputProps={{ style: { fontSize: "1.5rem" } }}
+            InputLabelProps={{ style: { fontSize: "1.3rem" } }}
+            {...register("titre")}
           />
         </FormControl>
       </Box>
-      <Box className='annonce-item'>
-        <Typography className='annonce-item-element' variant='h5'>
+      <Box className="annonce-item">
+        <Typography className="annonce-item-element" variant="h5">
           Description de l'annonce
         </Typography>
         <ReactQuill
-          className='annonce-item-element'
-          theme='snow'
-          value={value}
-          onChange={setValue}
+          className="annonce-item-element"
+          theme="snow"
+          value={description}
+          onChange={setDescription}
         />
+        <div className="description_error">
+          {" "}
+          {errors.description && errors.description?.message}
+        </div>
       </Box>
-      <Box className='annonce-item'>
-        <Typography className='annonce-item-element' variant='h5'>
+      <Box className="annonce-item">
+        <Typography className="annonce-item-element" variant="h5">
           Photos
         </Typography>
 
         <Grid
           container
-          className='annonce-item-element'
-          direction={'row'}
-          spacing={1}
-          justifyContent={'space-evenly'}
+          className="annonce-item-element"
+          direction={"column"}
+          sx={{ backgroundColor: "lightgrey" }}
+          height="300"
         >
-          <Grid
-            item
+          <Box
+            padding={1}
             sx={{
-              width: '240px',
-              height: '240px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
             }}
           >
-            <Paper
+            <Grid
+              item
               sx={{
-                width: '240px',
-                height: '240px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
+                width: "240px",
+                height: "240px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
               }}
-              variant='elevation'
             >
-              1
-            </Paper>
-          </Grid>
-          <Grid
-            item
-            sx={{
-              width: '240px',
-              height: '240px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
+              <Paper
+                sx={{
+                  width: "240px",
+                  height: "240px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                variant="elevation"
+              >
+                1
+              </Paper>
+            </Grid>
+            <Grid
+              item
+              sx={{
+                width: "240px",
+                height: "240px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Paper
+                sx={{
+                  width: "240px",
+                  height: "240px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                variant="elevation"
+              >
+                2
+              </Paper>
+            </Grid>
+            <Grid
+              item
+              sx={{
+                width: "240px",
+                height: "240px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Paper
+                variant="elevation"
+                sx={{
+                  width: 240,
+                  height: 240,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                3
+              </Paper>
+            </Grid>
+          </Box>
+          <Button
+            sx={{ margin: "0 auto" }}
+            variant="contained"
+            startIcon={<AddAPhotoIcon />}
+            component={"label"}
           >
-            <Paper
-              square
-              variant='elevation'
-              sx={{
-                width: 240,
-                height: 240,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              2
-            </Paper>
-          </Grid>
-          <Grid
-            item
-            sx={{
-              width: '240px',
-              height: '240px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Paper
-              square
-              variant='elevation'
-              sx={{
-                width: 240,
-                height: 240,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              3
-            </Paper>
-          </Grid>
+            Télécharger une photo
+            <input type="file" hidden />
+          </Button>
         </Grid>
       </Box>
-      <Box className='annonce-item'>
-        <Typography className='annonce-item-element' variant='h5'>
+      <Box className="annonce-item">
+        <Typography className="annonce-item-element" variant="h5">
           Adresse
         </Typography>
         <Grid
           container
-          direction={'column'}
-          justifyContent='flex-start'
-          maxWidth={'40%'}
+          direction={"column"}
+          justifyContent="flex-start"
+          maxWidth={"40%"}
         >
-          <Input placeholder='Adresse' />
-          <Input placeholder="Complément d'adresse" />
+          <TextField
+            variant="standard"
+            label="Adresse"
+            error={!!errors.adresse}
+            helperText={errors.adresse?.message}
+            {...register("adresse")}
+          />
+          <TextField
+            variant="standard"
+            label="Complément d'adresse"
+            error={!!errors.complement_adresse}
+            helperText={
+              errors.complement_adresse
+                ? errors.complement_adresse?.message
+                : "Ce champ est optionnel"
+            }
+            {...register("complement_adresse")}
+          />
           <Grid
-            flexDirection={'row'}
+            flexDirection={"row"}
             container
-            justifyContent={'space-between'}
+            justifyContent={"space-between"}
           >
-            <Input placeholder='Code postal' />
-            <Input placeholder='Ville' />
+            <TextField
+              variant="standard"
+              label="Code postal"
+              error={!!errors.code_postal}
+              helperText={errors.code_postal?.message}
+              {...register("code_postal")}
+            />
+            <TextField
+              variant="standard"
+              label="Ville"
+              error={!!errors.ville}
+              helperText={errors.ville?.message}
+              {...register("ville")}
+            />
           </Grid>
         </Grid>
       </Box>
-      <Box className='annonce-item'>
-        <Typography className='annonce-item-element' variant='h5'>
+      <Box className="annonce-item">
+        <Typography className="annonce-item-element" variant="h5">
           Contact
         </Typography>
-        <Input placeholder='Téléphone' />
+        <TextField
+          error={!!errors.telephone}
+          helperText={
+            errors.telephone
+              ? errors.telephone.message
+              : "Seulement les numéros mobiles sont acceptés"
+          }
+          variant="standard"
+          label="Téléphone"
+          {...register("telephone")}
+        />
       </Box>
       <Button
-        type='submit'
-        style={{ margin: '0 auto', display: 'flex' }}
-        variant='contained'
-        color='primary'
-        className='annonce-item'
+        type="submit"
+        style={{ margin: "0 auto", display: "flex" }}
+        variant="contained"
+        color="primary"
+        className="annonce-item"
         startIcon={<AddIcon />}
       >
         Soumettre
       </Button>
     </Container>
-  )
-}
+  );
+};
 
-export default Annonce
+export default Annonce;
