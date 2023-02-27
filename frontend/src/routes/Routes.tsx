7aa76b278c3503183d5a71d19055dc6e59c3ccda
Sandrine EAR <sandrine.ear@gmail.com>
2023-02-27 19:46:10 +0100

    Page liste des annonces sans données brutes

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Annonce from "../components/Annonce/Annonce";
 import Authentification from "../components/Authentification/Authentification";
 import Inscription from "../components/Inscription/Inscription";
-import ListeAnnonces from "../components/Annonces/ListeAnnonces";
+import ListeAnnonces from "../pages/Annonces/ListeAnnonces";
 const Router = () => (
   <Routes>
     <Route path="/annonces/ajouter" element={<Annonce />} />
