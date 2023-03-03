d6263416cdf7feb8a47e7fc416038c252fabb740
Sandrine EAR <sandrine.ear@gmail.com>
2023-03-03 10:48:07 +0100

    ajouter rtk + redux devtools

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-import {BrowserRouter} from 'react-router-dom'
-import { ThemeProvider } from '@mui/material/styles'
-import { theme } from './utils/theme'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./utils/theme";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Failed to find the root element')
-const root = createRoot(rootElement)
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <ThemeProvider theme={theme}>
-    <App />
-
-    </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-)
+);
